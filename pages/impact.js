96ceaf07a1f3c31f73ade17f992e7116592142db
Tanaya Nawathe <tnawathe@ucla.edu>
2021-08-04 18:41:10 -0700

    skeleton for impact page

---
+++ pages/impact.js
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import React from 'react';
+import Navigation from '../components/Committees/Sidebar';
+import Layout from '../components/Layout';
+import data from '../data';
+
+function ImpactPage() {
+
+	return (
+		<Layout>
+
+		</Layout>
+	);
+}
+
+export default ImpactPage;
